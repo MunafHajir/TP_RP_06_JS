cc437e45d62448ac764def8725233db0bdaa1337
Munaf Hajir <munafhajirpro@gmail.com>
2024-07-10 16:50:52 +0530

    Added Notes & ES6

--- intermediate/es6.js
+++ intermediate/es6.js
@@ -1,18 +1,144 @@
-let contactNo = [123, 1211, 1099, 1222];
+// Ecmascript
 
-// for (let i = 0; i < contactNo.length; i++) {
-//     contactNo[i] = '+91' + contactNo[i];
+// ES6
+// 1. LET / CONST / VAR
+// 2. Template Literals -> `${} `
+// 3. Array Methods -> Map, Filter, Reduce
+// 4. Arrow Function
+// 5. Spread operators
+// 6. Rest Parameters
+// 7. Destructuring
+// 8. Promises
+// 9. Async Await
+// 10. Class
+
+// function name () {
+//     //code
+//     return value
 // }
-// console.log(contactNo)
 
-// callback
-// function(value) {
-// return '+91' + value
+// Anonymous FUnction
+// function () {
+
 // }
 
-const contact = contactNo.map((value, idex) => {
-  console.log(value, idex);
-  return "+91" + value;
+// Function as a variable
+
+const greet = function () {
+  console.log("Hello");
+};
+
+greet();
+
+// Anonymous Function -> Callbacks
+
+// Arrow function
+
+// () => {}
+
+function add(a, b) {
+  return a + b;
+}
+
+const add2 = (a, b) => {
+  return a + b;
+};
+
+// 1. Syntactical Beautify 2. Lexical This
+
+1;
+const add3 = (a) => {
+  return a + 1;
+};
+
+2;
+
+const add4 = (a) => a + 1;
+
+// Normal Function -> Parent k this bind nhi hota h
+// Arrow Function -> Parent k this bibnd hota h
+
+// Spread Operator
+
+const batchDetails = { batchId: "TP_RP_05", mentor: "Munaf", tech: "ReactJS" };
+
+const sandeshStudent = {
+  name: "sandesh",
+  ...batchDetails,
+};
+
+console.log(sandeshStudent);
+
+// Rest Parameters
+
+function sum(...value) {
+  console.log(value);
+}
+
+sum(1, 2, 4, 5, 6, 7, 8);
+
+// Destructuring
+
+// 1. Object
+
+const { mentor, batchId, tech } = batchDetails;
+
+// batch["mentor"]
+// batch.mentor
+console.log(mentor, batchId, tech);
+
+// 2. Array
+
+const num = [1, 2, 3, 5, 6];
+
+// const first = num[0]
+// const second = num[1]
+
+let [first, second, , fourth] = num;
+console.log(first, second, fourth);
+
+const contactNo = [99999, 111111, 888888];
+
+// const updateValue = (value, index) => {
+//   return '+91' + value;
+// }
+
+const updatedContactNo = contactNo.map((value) => "+91" + value);
+
+console.log(updatedContactNo);
+
+contactNo.forEach((value) => {
+  console.log("for", value);
 });
 
-console.log(contact, contactNo);
+// includes
+// forEach
+// filter
+// reduce
+// splice
+// join
+
+// string method
+// object method
+
+// debugger;
+
+console.log("Munaf Arun Smita Sandesh".split(" "));
+
+function debug() {
+  try {
+    // Condition
+    // Loop
+    // API INtegrate
+    // SetTimeout
+  } catch (exception) {}
+}
+
+let response;
+const api = async () => {
+  response = await fetch("https://api.github.com/users/MunafHajir");
+  response = await response.json();
+  console.log(response);
+};
+
+api();
---
+++ intermediate/notes.md
@@ -0,0 +1,33 @@
+## We Covered in Intermediate HTML CSS JS
+
+# 1. HTML
+HTML Forms  -> form.html
+Selectors (global, tag, id, class) -> style.css
+CSS Resetting -> style.css
+Semantic Elements -> main.html
+Pseudoclass -> style.css
+Combinators -> style.css
+CSS naming convention -> style.css
+Position -> style.css
+Units -> style.css
+How to use tag and class selector for global setting -> style.css
+Variables -> style.css
+CSS Container -> style.css
+Grid Columns -> style.css
+CSS Animations -> style.css
+Media Query -> style.css
+CSS Roadmap / Frontend Interview Questions 
+Pseudoelements -> style.css
+
+## 2. ES6  -> es6.js
+Let vs Const vs Var
+Spread Operators
+Rest Parameters
+Arrow Function / Named Function / Function as a variable / Anonymous Function / Callback
+Shallow Copy / Deep Copy
+Template Literals
+Call by value / Call by reference
+Array Methods
+Debugging / Error Handling
+API Integration
+OOP
