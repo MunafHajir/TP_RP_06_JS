f54feb6077d4547ebf1bd1585bef3d5e523c588d
Munaf Hajir <munafhajirpro@gmail.com>
2024-07-18 20:46:54 +0530

    Pillars of OOP

--- intermediate/oop.js
+++ intermediate/oop.js
@@ -3,32 +3,74 @@
 // is JS -> Fully Object Oriented -> No
 // class, constructor, new, this
 
-
 // template & blueprint of an object
-class Student {
+// class Student {
 
-    // automatically call on creating an object
-    constructor(first_name, last_name, mobile, batch_code, tech) {
-        // current object
-        this.name =  first_name + " " + last_name;
-        this.mobile = mobile;
-        this.tech = tech;
-    }
+//     // automatically call on creating an object
+//     constructor(first_name, last_name, mobile, batch_code, tech) {
+//         // current object
+//         this.name =  first_name + " " + last_name;
+//         this.mobile = mobile;
+//         this.tech = tech;
+//     }
 
-    updateMobile(mobile) {
-       this.mobile =  mobile;
-    }
+//     updateMobile(mobile) {
+//        this.mobile =  mobile;
+//     }
 
-    addTech(tech) {
-        this.tech.push(tech);   
-    }
+//     addTech(tech) {
+//         this.tech.push(tech);
+//     }
+// }
+
+// // Object Creating
+// const shadab = new Student("Shadab", "Ansari", "197917893791", "TP_RP_06", ["HTML", "CSS", "JS"]);
+// const sheela = new Student("Sheela", "Mishra", "197917893791", "TP_RP_06", ["HTML", "CSS", "JS"]);
+
+// shadab.addTech("ReactJS")
+
+// console.log(shadab);
+
+class Account {
+  #balance;
+  constructor(id, name, balance) {
+    this.id = id;
+    this.name = name;
+    this.#balance = balance;
+  }
+
+  depositAmount(amount) {
+    this.#balance += amount;
+    return this.#balance;
+  }
+
+  withdrawAmount(amount) {
+    this.#balance -= amount;
+    return this.#balance;
+  }
+
+  getBalance() {
+    return this.#balance;
+  }
 }
 
-// Object Creating
-const shadab = new Student("Shadab", "Ansari", "197917893791", "TP_RP_06", ["HTML", "CSS", "JS"]);
-const sheela = new Student("Sheela", "Mishra", "197917893791", "TP_RP_06", ["HTML", "CSS", "JS"]);
-const sheela = new Student("Sheela", "Mishra", "197917893791", "TP_RP_06", ["HTML", "CSS", "JS"]);
+class CurrentAccount extends Account{
+  calculateInterest() {
+    return 0
+  }
+}
 
-shadab.addTech("ReactJS")
+class SavingAccount extends Account {
+  calculateInterest() {
+    return this.getBalance() * 2.5 / 100; 
+  }
+}
 
-console.log(shadab);
+const samruddhiAccount = new CurrentAccount(123, "Samruddhi", 0);
+const sarojAccount = new SavingAccount(123, "Saroj", 1);
+
+sarojAccount.depositAmount(10000);
+samruddhiAccount.depositAmount(2000);
+
+console.log(samruddhiAccount.calculateInterest());
+console.log(sarojAccount.calculateInterest());
